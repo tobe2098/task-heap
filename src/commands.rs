9a539f5ca5715b14103cb8ca17b730768ea8508c
Antoni Bertolin Monferrer <antoni.monferrer@canonical.com>
2026-01-30 00:54:52 +0100

    [cmd] Add new commands and behaviors

--- src/commands.rs
+++ src/commands.rs
@@ -1,46 +1,80 @@
+use crate::utils::Weight;
 pub enum Commands {
-    Push(String),
+    CreateStack(String),
+    DestroyStack(String),
+    PushTask(String),      //Push task onto stack.
+    PopTask,               //Pop task from staged tasks.
+    InsertTask(String),    //Indexed or by name.
+    RemoveTask(String),    //||
+    Edit(String),          //Both stack or task and the stack is the argument.
+    StageTask(String),     //Arg is stack always
+    UnstageTask(String),   //Stage or unstage
+    ClearDone(String),     //Arg is stack
+    ClearAllTasks(String), //Arg is stack
+    List(Option<String>),  //Either a specific stack, or stacks only
+    Help,
     Name(String),
     Description(String),
-    Weight(String),
+    Weight(Weight),
     Tag(Vec<String>),
     Untag(Vec<String>),
-    ClearTags(String),
-    Pop,
-    Delete(String),
-    Edit(String),
-    List,
-    Reset,
-    Help,
+    Index(usize),
+    Stack(String),
+    Task(String),
 }
+//Add create, destroy (stack), delete is indexed (if no index it is a pop), insert is indexed (if no index it is a push) as well, and per stack.
+//Two states, staged and unstaged.
+//By default staged is the first task only, to stage more you need to specify
+//Randomly popping is from staged piles of each stack
+//When listing, show the stacks and their staged tasks, if list --stack name print the whole
+//info about stack. list --all prints everything.
+//Clear stack of finished tasks
+//Push pop arg are for the stacks
+//Stage unstage
+//Do I move away from -- actions? No more chaining arguments?
+//. action name --option aasdw daw daw d --option
+//naming: stack:task because of edit? or edit-task vs edit-stack? Same problem.  edit vs
+//edit stack --task name --options
 impl Commands {
-    pub fn needs_non_empty_heap(&self) -> bool {
-        matches!(
-            self,
-            Self::ClearTags(_) | Self::Pop | Self::Delete(_) | Self::Edit(_) | Self::List
-        )
-    }
     pub fn is_valid_for(&self, command: &Commands) -> bool {
         match (command, self) {
             // Push accepts everything except Untag
-            (Commands::Push(_), Self::Description(_) | Self::Weight(_) | Self::Tag(_)) => true,
+            (Self::CreateStack(_), Self::Description(_) | Self::Weight(_) | Self::Tag(_)) => true,
+            (Self::DestroyStack(_), Self::Tag(_)) => true,
+            (
+                Self::PushTask(_),
+                Self::Name(_) | Self::Description(_) | Self::Weight(_) | Self::Tag(_),
+            ) => true,
 
             // Pop/Delete ONLY accept filtering tags
-            (Commands::Pop, Self::Tag(_)) => true,
-            (Commands::Delete(_), Self::Tag(_)) => true,
+            (Self::PopTask, Self::Tag(_) | Self::Stack(_)) => true,
+
+            (
+                Self::InsertTask(_),
+                Self::Index(_)
+                | Self::Name(_)
+                | Self::Description(_)
+                | Self::Weight(_)
+                | Self::Tag(_),
+            ) => true,
+
+            (Self::RemoveTask(_), Self::Name(_) | Self::Index(_)) => true,
 
             // Edit accepts specific fields
             (
-                Commands::Edit(_),
+                Self::Edit(_),
                 Self::Name(_)
+                | Self::Task(_)
                 | Self::Description(_)
                 | Self::Weight(_)
                 | Self::Tag(_)
                 | Self::Untag(_),
             ) => true,
 
+            (Self::StageTask(_), Self::Task(_)) => true, //Arg is stack always
+            (Self::UnstageTask(_), Self::Task(_)) => true, //Arg i or unstage
             //List accepts tag and weight (for now equal, but <> in future)
-            (Commands::List, Self::Tag(_) | Self::Weight(_)) => true,
+            (Self::List(_), Self::Tag(_)) => true,
 
             // Default to false for everything else
             _ => false,
